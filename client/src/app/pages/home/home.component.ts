96d65c9b439d503b12db116aba47683628eabaec
Davide Chigliaro <davide.chigliaro@grupposcai.it>
2025-03-08 20:42:50 +0100

    Feat: added home and common components

---
+++ client/src/app/pages/home/home.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+import { SharedModule } from "primeng/api";
+
+@Component({
+  selector: "app-home",
+  imports: [SharedModule],
+  templateUrl: "./home.component.html",
+  styles: ``,
+})
+export class HomeComponent {}
